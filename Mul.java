f2652b89c738b5bdb22bc06f8a86da0049226dd5
surge1 <surgesoftware@gmail.com>
2023-09-29 17:58:29 +0530

    Changes on Add.java& Mul.java

--- Mul.java
+++ Mul.java
@@ -2,6 +2,6 @@ public class Mul {
     public static void main(String[] args) {
         int num1=10;
         int num2 =20;
-       System.out.println(num1/num2);
+       System.out.println(num1+num2);
     }
 }
